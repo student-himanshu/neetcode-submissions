358b8570e9d4fca287d3b8982c553164517bef1c
Himanshu mishra <144526784+student-himanshu@users.noreply.github.com>
2026-08-16 14:17:28 +0530

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq(stones.begin(),stones.end());
+        if(pq.size()<2)return pq.top();
+        while(pq.size()>1){
+            int a=pq.top();
+            pq.pop();
+            int b=pq.top();
+            pq.pop();
+            if(a==b)continue;
+            else if(a>=b)pq.push(a-b);
+        }
+        return (pq.size()<1)?0:pq.top();
+    }
+};
