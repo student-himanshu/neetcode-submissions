f1bf32f3baaafccd83cdfe0c7468368a6cadfd6e
Himanshu mishra <144526784+student-himanshu@users.noreply.github.com>
2026-08-18 19:13:09 +0530

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    // private:
+    // bool bfs(int l,int r,vector<vector<int>>& matrix, int target){
+    //     if(matrix[l][r]==target){
+    //         return true;
+    //     }
+    //     int mid=l+(r-l)/2;
+
+    // }
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows=matrix.size(),cols=matrix[0].size();
+        int top=0,bot=rows-1;
+        while(top<=bot){
+            int row=(top+bot)/2;
+            if(target>matrix[row][cols-1]){
+                top=row+1;
+            }
+            else if(target<matrix[row][0]){
+                bot=row-1;
+            }
+            else{
+                break;
+            }
+
+        }
+        if(!(top<=bot)){
+            return false;
+        }
+        int row=(top+bot)/2;
+        int l=0,r=cols-1;
+        while(l<=r){
+            int m=(l+r)/2;
+            if(target>matrix[row][m]){
+                l=m+1;
+            }else if(target<matrix[row][m]){
+                r=m-1;
+            }
+            else return true;
+        }
+        return false;
+    }
+};
